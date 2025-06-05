2009c333eaa7b3b3c0e50c209543e02d18b6f790
nuresuke <s2270668@s.do-johodai.ac.jp>
2025-06-05 15:52:49 +0900

    models.pyの変更

--- Akinator/models.py
+++ Akinator/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Pokemon(models.Model):
+    zukan_no = models.IntegerField()
+    name = models.CharField(max_length=20)
+    type = models.CharField(max_length=10)
+    color = models.CharField(max_length=20)
+    weight = models.FloatField()
+    height = models.FloatField()
+    evolution = models.IntegerField()
+    can_fly = models.BooleanField()
+    habitat = models.CharField(max_length=50)
+    is_legendary = models.BooleanField()
+    feature = models.CharField(max_length=50)
+    is_fossil = models.BooleanField()
+    has_special_skill = models.BooleanField()
+    has_sash = models.BooleanField()
+    characteristic = models.CharField(max_length=10)
+    initial = models.CharField(max_length=1)
