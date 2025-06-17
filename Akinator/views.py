77aa759ccfb756d0e221d82bbb8341b35adf8b79
nuresuke <s2270668@s.do-johodai.ac.jp>
2025-06-17 09:06:10 +0900

    外形作成

--- Akinator/views.py
+++ Akinator/views.py
@@ -1,5 +1,51 @@
 from django.shortcuts import render
+from .models import Pokemon
+import tensorflow as tf
+import numpy as np
 
 # Create your views here.
 def index_view(request):
     return render(request, "interface/index.html")
+
+#文字列のベクトル化
+def one_hot_encode_type(type_str):
+    type_list = ['みず', 'ほのお', 'くさ','ノーマル', 'ほのお', 'みず', 'くさ', 'でんき', 'こおり', 'かくとう', 'どく', 'じめん', 'ひこう',
+                 'エスパー', 'むし', 'いわ', 'ゴースト', 'ドラゴン', 'あく', 'はがね', 'フェアリー']  # すべてのタイプを列挙
+    vec = [1 if type_str == t else 0 for t in type_list]
+    return vec
+def one_hot_encode_color(color_str):
+    color_list = ['あか', 'あお', 'きいろ', 'みどり', 'くろ', 'しろ', 'ちゃいろ', 'オレンジ','むらさき', 'グレー', 'ピンク']
+    vec = [1 if color_str == c else 0 for c in color_list]
+    return vec
+
+def pokemon_list_view(request):
+    # 1. DBから全ポケモン取得
+    pokemons = Pokemon.objects.all()
+
+    # 2. データをTensorFlow用に整形
+    features = []
+    for p in pokemons:
+            feature = []
+            feature.extend(one_hot_encode_type(p.type)),     # 文字列→One-Hot
+            feature.extend(one_hot_encode_color(p.color)),    # 文字列→One-Hot
+            feature.extend([
+            p.weight,
+            p.height,
+            int(p.can_fly),
+            int(p.is_legendary),
+            int(p.is_fossil),
+            int(p.has_special_skill),
+            int(p.has_sash),
+        ])
+            features.append(feature)
+    features = np.array(features)
+
+    # 3. TensorFlowで絞り込み
+    model = tf.keras.models.load_model('model_path')
+    preds = model.predict(features)
+
+    # しきい値を設定して、条件を満たすものだけ抽出
+    filtered_pokemons = [p for p, pred in zip(pokemons, preds) if pred > 0.95]
+
+    # 4. テンプレートに渡して表示
+    return render(request, "interface/pokemon_list.html", {"pokemons": filtered_pokemons})
